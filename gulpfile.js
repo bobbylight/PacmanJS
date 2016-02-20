2e046d9fdd4ee12407be8b73ce948de088340d72
bobbylight <robert.e.futrell@gmail.com>
2016-02-19 23:51:27 -0500

    Fixing indentation

--- gulpfile.js
+++ gulpfile.js
@@ -1,50 +1,52 @@
 (function() {
-   'use strict';
+    'use strict';
 
-   var gulp = require('gulp');
-   var debug = require('gulp-debug');
-   var del = require('del');
-   var runSequence = require('run-sequence');
-   var usemin = require('gulp-usemin');
-   var uglify = require('gulp-uglify');
-   var concatCss = require('gulp-concat-css');
-   var cssmin = require('gulp-cssmin');
-   var rev = require('gulp-rev');
-   var tsc = require('gulp-typescript');
-   var tsconfig = tsc.createProject('tsconfig.json');
-   var sourcemaps = require('gulp-sourcemaps');
-   var tslint = require('gulp-tslint');
-   var replace = require('gulp-replace');
-   var dateFormat = require('dateformat');
+    var gulp = require('gulp');
+    var debug = require('gulp-debug');
+    var del = require('del');
+    var runSequence = require('run-sequence');
+    var usemin = require('gulp-usemin');
+    var uglify = require('gulp-uglify');
+    var concatCss = require('gulp-concat-css');
+    var cssmin = require('gulp-cssmin');
+    var rev = require('gulp-rev');
+    var tsc = require('gulp-typescript');
+    var tsconfig = tsc.createProject('tsconfig.json');
+    var sourcemaps = require('gulp-sourcemaps');
+    var tslint = require('gulp-tslint');
+    var replace = require('gulp-replace');
+    var dateFormat = require('dateformat');
+    var electron = require('gulp-electron');
+    var packageJson = require('./package.json');
 
-   gulp.task('clean', function() {
-      return del([
-         './dist'
-      ]);
-   });
+    gulp.task('clean', function() {
+        return del([
+            './dist'
+        ]);
+    });
 
-   gulp.task('usemin', function() {
-      return gulp.src([ 'src/index.html' ])
-         .pipe(debug({ title: 'File going through usemin: ' }))
-         .pipe(usemin({
-            css: [ rev ],
-            js: [ uglify, rev ],
-            inlinejs: [ uglify ]
-            //, html: [ minifyHtml({ empty: true }) ]
-         }))
-         .pipe(replace('<%=build.date%>',
-            dateFormat(new Date(), 'dddd, mmmm dS, yyyy, h:MM:ss TT')))
-         .pipe(gulp.dest('dist/'));
-   });
+    gulp.task('usemin', function() {
+        return gulp.src([ 'src/index.html' ])
+            .pipe(debug({ title: 'File going through usemin: ' }))
+            .pipe(usemin({
+                css: [ rev ],
+                js: [ uglify, rev ],
+                inlinejs: [ uglify ]
+                //, html: [ minifyHtml({ empty: true }) ]
+            }))
+            .pipe(replace('<%=build.date%>',
+                dateFormat(new Date(), 'dddd, mmmm dS, yyyy, h:MM:ss TT')))
+            .pipe(gulp.dest('dist/'));
+    });
 
-   gulp.task('cssmin', function() {
-      gulp.src('src/css/all.css')
-         .pipe(concatCss('all.css'))
-         .pipe(cssmin())
-         .pipe(gulp.dest('dist/css/'));
-   });
+    gulp.task('cssmin', function() {
+        gulp.src('src/css/all.css')
+            .pipe(concatCss('all.css'))
+            .pipe(cssmin())
+            .pipe(gulp.dest('dist/css/'));
+    });
 
-   gulp.task('compile-ts', function() {
+    gulp.task('compile-ts', function() {
         var tsResult = gulp.src([ 'src/app/**/*.ts', 'bower_components/gtp/dist-all/gtp-all.d.ts', 'src/typings/**/*.ts' ])
             .pipe(sourcemaps.init())
             .pipe(tsc(tsconfig));
@@ -52,24 +54,27 @@
         return tsResult.js
             .pipe(sourcemaps.write('.'))
             .pipe(gulp.dest('src/js/'));
-   });
-   gulp.task('tslint', function() {
-      return gulp.src([ 'src/app/**/*.ts' ])
-          .pipe(tslint())
-          .pipe(tslint.report('prose'));
-   });
+    });
+    gulp.task('tslint', function() {
+        return gulp.src([ 'src/app/**/*.ts' ])
+            .pipe(tslint())
+            .pipe(tslint.report('prose'));
+    });
 
-   gulp.task('copy-non-minified-files', function() {
-      return gulp.src([ 'src/**', 'src/.htaccess', '!src/css/**', '!src/js/**', '!src/{app,app/**}', '!src/{typings,typings/**}', '!src/index.html' ])
-         .pipe(gulp.dest('dist/'));
-   });
+    gulp.task('copy-non-minified-files', function() {
+        return gulp.src([ 'src/**', 'src/.htaccess', '!src/css/**', '!src/js/**', '!src/{app,app/**}', '!src/{typings,typings/**}', '!src/index.html' ])
+            .pipe(gulp.dest('dist/'));
+    });
 
-   gulp.task('default', function() {
-      runSequence('tslint', 'clean', 'compile-ts', 'usemin', 'cssmin', 'copy-non-minified-files');
-   });
+    gulp.task('default', function() {
+        runSequence('tslint', 'clean', 'compile-ts', 'usemin', 'cssmin', 'copy-non-minified-files');
+    });
 
-   gulp.task('watch', [ 'tslint', 'compile-ts'], function() {
-      gulp.watch('src/app/**/*.ts', [ 'tslint', 'compile-ts' ]);
-   });
+    gulp.task('watch', [ 'tslint', 'compile-ts'], function() {
+        gulp.watch('src/app/**/*.ts', [ 'tslint', 'compile-ts' ]);
+    });
 
+    gulp.task('electron', function() {
+        // gulp-electron does not seem to work, unfortunately
+    });
 })();
