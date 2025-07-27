de7b11becdb3c1a2041fd49d50aebb02a517b565
bobbylight <robert.e.futrell@gmail.com>
2025-07-27 17:53:29 -0400

    Fix #16: Ghosts never return from blue state

--- src/pacman/Ghost.spec.ts
+++ src/pacman/Ghost.spec.ts
@@ -1,14 +1,19 @@
-import { Ghost } from './Ghost';
+import { Ghost, MotionState } from './Ghost';
 import { Maze } from './Maze';
 import { PacmanGame } from './PacmanGame';
-import { describe, expect, test } from 'vitest';
+import { describe, expect, it } from 'vitest';
 
-const game: any = {};
+const game: any = {
+    checkLoopedSound: () => {},
+    level: 0,
+    playTime: 5000,
+};
 
 export class ConcreteGhost extends Ghost {
 
     constructor() {
         super(game as PacmanGame, 0, 0);
+        this.motionState = MotionState.CHASING_PACMAN;
     }
 
     protected updatePositionChasingPacman(maze: Maze): void {
@@ -17,8 +22,29 @@ export class ConcreteGhost extends Ghost {
 
 describe('Ghost', () => {
 
-    test('can be subclassed', () => {
+    it('can be subclassed', () => {
         const ghost: ConcreteGhost = new ConcreteGhost();
         expect(ghost).toBeDefined();
+    });
+
+    describe('possiblyTurnBlue', () => {
+        [ MotionState.EYES, MotionState.EYES_ENTERING_BOX, MotionState.IN_BOX, MotionState.LEAVING_BOX ].forEach(state => {
+            it(`does nothing if they are ${state}`, () => {
+                const ghost: ConcreteGhost = new ConcreteGhost();
+                ghost.motionState = state;
+                ghost.possiblyTurnBlue();
+                expect(ghost.isBlue()).toBe(false);
+            });
+        });
+
+        [ MotionState.SCATTERING, MotionState.CHASING_PACMAN, MotionState.BLUE ].forEach(state => {
+            it(`turns blue if they are ${state}`, () => {
+                const ghost: ConcreteGhost = new ConcreteGhost();
+                ghost.motionState = state;
+                ghost.possiblyTurnBlue();
+                expect(ghost.isBlue()).toBe(true);
+                expect(ghost.exitBlueTime).toBeGreaterThan(0); // Fix #16
+            });
+        });
     });
 });
