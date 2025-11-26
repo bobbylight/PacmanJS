bc11885057b872c59c3db8a050c78e8b7ce471c2
bobbylight <robert.e.futrell@gmail.com>
2025-11-25 22:39:06 -0500

    build(deps): bump vitest to 4.x and other minor dependency updates

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ export default defineConfig({
     plugins: [],
     test: {
         environment: 'jsdom',
+        globals: true,
         coverage: {
             reporter: ['text', 'json', 'lcov'],
             reportOnFailure: true,
